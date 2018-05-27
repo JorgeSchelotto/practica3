64ff750ec939956910598fd62f9ed43401445a61
Jorge Schelotto <jasfotografo@hotmail.com>
2018-05-26 23:41:01 -0300

    'Segundo'

---
+++ Ejercicio3Json.py
@@ -0,0 +1,33 @@
+import json
+"""Registrar los jugadores del Ejercicio 7 de la Práctica 1 en un archivo utilizando cualquiera de
+las librerías dadas en la teoría (Pickle, JSON, CSV). Implementar una función denominada
+modificoDatos, la cual solicita por teclado los datos de un jugador, si este existe en el
+archivo, modifica dichos datos en el mismo. Si no existe el jugador, lo agrega."""
+
+jugadores = {}
+stats = {}
+
+jugador = input('Ingrese Nombre del jugador. Ingrese cancelar para cortar la carga: ')
+
+while(jugador != 'cancelar'):
+    nivel = input('Ingrese Nivel del jugador: ')
+    puntaje = int(input('Ingrese puntaje del jugador: '))
+    horasJuego = input('Ingrese horas de juego del jugador: ')
+
+    stats['nivel'] = nivel
+    stats['puntaje'] = puntaje
+    stats['horas'] = horasJuego
+    jugadores[jugador] = stats
+
+    jugador = input('Ingrese Nombre del jugador: ')
+
+
+file = open('json.txt', 'w')
+json.dump(jugadores,file)
+
+
+file = open('json.txt', 'r')
+for dato in file:
+    print(dato)
+
+file.close()
